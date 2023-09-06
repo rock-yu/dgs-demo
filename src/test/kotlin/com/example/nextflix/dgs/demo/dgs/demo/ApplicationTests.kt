1a903b196fb4ebc589f758fbb8da2e463ff85f61
Rock Yu <rock.yu@myob.com>
2023-09-06 18:11:14 +1000

    Initial commit with a demo for Netflix DSG example following https://netflix.github.io/dgs/getting-started/

---
+++ src/test/kotlin/com/example/nextflix/dgs/demo/dgs/demo/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nextflix.dgs.demo.dgs.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
